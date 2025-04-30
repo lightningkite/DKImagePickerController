ede25348249d5d75ebd9647789f915e20bb2d5be
Shane Thompson <shane@meosphere.com>
2025-04-30 15:52:00 -0600

    Package updates

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["DKImagePickerController"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/zhangao0086/DKCamera", .branch("master")),
-        .package(url: "https://github.com/zhangao0086/DKPhotoGallery", .branch("master")),
+        .package(url: "https://github.com/lightningkite/DKCamera", exact: "1.6.11"),
+        .package(url: "https://github.com/zhangao0086/DKPhotoGallery", branch: "master"),
         .package(url: "https://github.com/TimOliver/TOCropViewController", from: "2.6.0"),
     ],
     targets: [
